fa7ca3af9252f86975e7587e6bfd221f7f7daa76
giali.coletta <51245135+Gialicus@users.noreply.github.com>
2024-12-17 17:35:54 +0100

    store

--- src/Edg3Store.ts
+++ src/Edg3Store.ts
@@ -4,25 +4,22 @@ import { Nod3Id } from "./Nod3";
 export interface Edg3Store<T> {
   init(id: Nod3Id): void;
   addEdge(from: Nod3Id, to: Nod3Id, data: CreateEdg3Input<T>): void;
-  readInEdgesById(id: string): Edg3<T>[];
-  readOutEdgesById(id: string): Edg3<T>[];
+  addInverseEdge(from: Nod3Id, to: Nod3Id, data: CreateEdg3Input<T>): void;
+  readEdges(id: string): Edg3<T>[];
 }
 
 export class Edg3MemoryStore<T> implements Edg3Store<T> {
-  inEdges: Map<Nod3Id, Map<Nod3Id, Edg3<T>>> = new Map();
-  outEdges: Map<Nod3Id, Map<Nod3Id, Edg3<T>>> = new Map();
+  edge: Map<Nod3Id, Map<Nod3Id, Edg3<T>>> = new Map();
   init(id: Nod3Id): void {
-    this.inEdges.set(id, new Map());
-    this.outEdges.set(id, new Map());
+    this.edge.set(id, new Map());
   }
   addEdge(from: Nod3Id, to: Nod3Id, data: CreateEdg3Input<T>): void {
-    this.inEdges.get(to)!.set(from, { in: from, out: to, ...data });
-    this.outEdges.get(from)!.set(to, { in: from, out: to, ...data });
+    this.edge.get(to)!.set(from, { in: from, out: to, ...data });
   }
-  readInEdgesById(id: string): Edg3<T>[] {
-    return [...this.inEdges.get(id)!.values()];
+  addInverseEdge(from: Nod3Id, to: Nod3Id, data: CreateEdg3Input<T>): void {
+    this.edge.get(from)!.set(to, { in: from, out: to, ...data });
   }
-  readOutEdgesById(id: string): Edg3<T>[] {
-    return [...this.outEdges.get(id)!.values()];
+  readEdges(id: string): Edg3<T>[] {
+    return [...this.edge.get(id)!.values()];
   }
 }
--- src/Graffio.ts
+++ src/Graffio.ts
@@ -5,12 +5,14 @@ import { Nod3MemoryStore, Nod3Store } from "./Nod3Store";
 
 export class Graffio<T = unknown, K = unknown> {
   nodes: Nod3Store<T> = new Nod3MemoryStore();
-  edges: Edg3Store<K> = new Edg3MemoryStore();
+  inEdges: Edg3Store<K> = new Edg3MemoryStore();
+  outEdges: Edg3Store<K> = new Edg3MemoryStore();
 
   addNode(node: CreateNod3Input<T>): string {
     const id = generateId();
     this.nodes.set(id, { id: id, ...node });
-    this.edges.init(id);
+    this.inEdges.init(id);
+    this.outEdges.init(id);
     return id;
   }
 
@@ -18,29 +20,30 @@ export class Graffio<T = unknown, K = unknown> {
     if (!this.nodes.has(from) || !this.nodes.has(to)) {
       throw new Error(`Nodes from: ${from} or to: ${to} not exist`);
     }
-    this.edges.addEdge(from, to, data);
+    this.inEdges.addEdge(from, to, data);
+    this.outEdges.addInverseEdge(from, to, data);
     return this;
   }
 
   readInNeighbours(id: Nod3Id): Edg3<K>[] {
-    return this.edges.readInEdgesById(id);
+    return this.inEdges.readEdges(id);
   }
 
   readOutNeighbours(id: Nod3Id): Edg3<K>[] {
-    return this.edges.readOutEdgesById(id);
+    return this.outEdges.readEdges(id);
   }
 
   print(): void {
     for (const node of this.nodes.values()) {
       console.log("-".repeat(72));
       console.log(`(${node.label}:${node.id})\n`);
-      for (const ins of this.edges.readInEdgesById(node.id)) {
+      for (const ins of this.inEdges.readEdges(node.id)) {
         const inNode = this.nodes.get(ins.in)!;
         console.log(
           `\t(${node.label}:${node.id}) <- ${ins.label} -- (${inNode.label}:${inNode.id})`
         );
       }
-      for (const out of this.edges.readOutEdgesById(node.id)) {
+      for (const out of this.outEdges.readEdges(node.id)) {
         const outNode = this.nodes.get(out.out)!;
         console.log(
           `\t(${node.label}:${node.id}) -- ${out.label} -> (${outNode.label}:${outNode.id})`
